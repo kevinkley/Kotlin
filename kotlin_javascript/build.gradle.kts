bde13d32f8823bc85c0c2bfc1d403991db4213d5
kevinkley <kevinkleyoficial@gmail.com>
2023-07-08 14:47:58 -0300

    envio

---
+++ kotlin_javascript/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    kotlin("js") version "1.8.21"
+}
+
+group = "me.kevin"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+kotlin {
+    js {
+        binaries.executable()
+        browser {
+            commonWebpackConfig {
+                cssSupport {
+                    enabled.set(true)
+                }
+            }
+        }
+    }
+}
---
+++ kotlin_javascript/src/main/kotlin/Simple.kt
@@ -0,0 +1,7 @@
+fun main() {
+    val aumento = 10
+}
+
+fun forca(soco: Int, chute: Int): Int{
+    return soco + chute
+}
