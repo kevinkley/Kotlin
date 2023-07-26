773c45c4289e96be27e0c18913af6ba7ec7107e9
kevinkley <kevinkleyoficial@gmail.com>
2023-07-26 01:18:48 -0300

    atualizacao

--- EstruturadeControle/build.gradle.kts
+++ EstruturadeControle/build.gradle.kts
@@ -15,6 +15,7 @@ repositories {
 dependencies {
     testImplementation(kotlin("test"))
     implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
+    testImplementation ("junit:junit:4.13.2")
 }
 
 tasks.test {
--- EstruturadeControle/src/main/kotlin/EstruturadeRepeticao/While/while.kt
+++ EstruturadeControle/src/main/kotlin/EstruturadeRepeticao/While/while.kt
@@ -1,2 +1,15 @@
-package EstruturadeRepeticao.While
 
+fun main() {
+    var condicao = true
+
+    while(condicao){
+        println("Digite um numero: ")
+        val entrada = readLine()?.toIntOrNull()
+
+        if(entrada != null) {
+            println("O nome digitado foi: $entrada")
+        }else{
+            condicao = false
+        }
+    }
+}
