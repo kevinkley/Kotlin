00459837d2dfe1c5da7bdf2e5ef642d008b3770b
kevinkley <kevinkleyoficial@gmail.com>
2023-07-26 22:14:42 -0300

    while

--- EstruturadeControle/src/main/kotlin/EstruturadeRepeticao/While/while.kt
+++ EstruturadeControle/src/main/kotlin/EstruturadeRepeticao/While/while.kt
@@ -1,15 +1,31 @@
+import jdk.swing.interop.SwingInterOpUtils
 
 fun main() {
     var condicao = true
+    var valor1: Int
+    var valor2: Int
+    var operador: String
+    var resultado: Int
+    var receberResultado = 0
+    do{
+        var soma = 0
 
-    while(condicao){
-        println("Digite um numero: ")
-        val entrada = readLine()?.toIntOrNull()
+        println("Digite um operador: ")
+        operador = readln()
 
-        if(entrada != null) {
-            println("O nome digitado foi: $entrada")
+        if(operador == "+"){
+            println("Digite um valor: ")
+            valor1 = readln().toIntOrNull() ?: 0
+            println("Digite um valor: ")
+            valor2 = readln().toIntOrNull() ?: 0
+
+            resultado = valor1.plus(valor2)
+            soma += resultado
         }else{
             condicao = false
         }
-    }
+
+        receberResultado += soma
+        println("soma é $receberResultado")
+    }while(condicao)
 }
